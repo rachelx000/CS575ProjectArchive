e81780bb2098404afae48a94c9fb113dd71fe59f
Rachel Xing <xingru@flip1.engr.oregonstate.edu>
2025-04-08 23:30:51 -0700

    Upload probability results and performance outputs

--- Project1/Project1.bash
+++ Project1/Project1.bash
@@ -3,7 +3,7 @@
 #number of threads:
 for t in 1 2 4 8
 do
-    for n in 100 1000 10000 50000 100000
+    for n in 1 10 100 1000 10000 100000 500000
     do
       # echo NUMT = $t
       g++ -DNUMT=$t -DNUMTRIALS=$n Project1.cpp -o Project1  -lm -fopenmp
