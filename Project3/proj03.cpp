53d70235c23175c1468560c82ec31cb60a99bfb2
Rachel Xing <xingru@rabbit.engr.oregonstate.edu>
2025-05-03 00:28:09 -0700

    Update result.csv for Project #3

--- Project3/proj03.cpp
+++ Project3/proj03.cpp
@@ -191,7 +191,7 @@ main( int argc, char *argv[ ] )
 		}
 	}
 #ifdef CSV
-        fprintf(stderr, "%2d , %4d , %4d , %8.3lf:\n", NUMT, (int) NUMCITIES, NUMCAPITALS, megaCityCapitalsPerSecond );
+        fprintf(stderr, "%2d , %4d , %4d , %8.3lf\n", NUMT, (int) NUMCITIES, NUMCAPITALS, megaCityCapitalsPerSecond );
 #else
         fprintf(stderr, "%2d threads : %4d cities ; %4d capitals; megatrials/sec = %8.3lf\n",
                 NUMT, (int) NUMCITIES, NUMCAPITALS, megaCityCapitalsPerSecond );
