e891fe1b79d9a104a7b8bf573fd88565b987a1ce
Rachel Xing <xingru@rabbit.engr.oregonstate.edu>
2025-05-19 04:55:12 -0700

    Update Project #6 results

--- Project6/submit.bash
+++ Project6/submit.bash
@@ -3,13 +3,13 @@
 #SBATCH  -A  cs475-575
 #SBATCH  -p  classgpufinal
 #SBATCH  --constraint=v100
-#SBATCH  --gres=gpu:1
+#SBATCH  --gres=gpu:2
 #SBATCH  -o  proj06.out
 #SBATCH  -e  proj06.err
 #SBATCH  --mail-type=BEGIN,END,FAIL
 #SBATCH  --mail-user=xingru@oregonstate.edu
 
-for s in 4096 16384 65536 262144 1048576 4194304
+for s in 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304
 do
         for b in 8 32 64 128 256
         do
