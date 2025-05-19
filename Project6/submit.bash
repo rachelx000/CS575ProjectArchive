2e39d8003ebf224db2f7faf1292d2dc48de1d76c
Rachel Xing <xingru@submit-c.ib.coehpc>
2025-05-19 02:10:25 -0700

    Upload running results on DGX for Project #6

--- Project6/submit.bash
+++ Project6/submit.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH  -J  Proj06
 #SBATCH  -A  cs475-575
-#SBATCH  -p  classgputest
+#SBATCH  -p  classgpufinal
 #SBATCH  --constraint=v100
 #SBATCH  --gres=gpu:1
 #SBATCH  -o  proj06.out
