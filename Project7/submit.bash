4273105fa1cc14501feb71de997f03b8c1ce113c
rachelx000 <rachelxing979@gmail.com>
2025-05-25 01:59:18 -0700

    Initialization of Project #7

---
+++ Project7/submit.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -J Fourier
+#SBATCH -A cs475-575
+#SBATCH -p classmpitest
+#SBATCH -N 8
+#SBATCH -n 8
+#SBATCH -o fourier.out
+#SBATCH -e fourier.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=xingru@oregonstate.edu
+
+module load slurm
+module load openmpi
+mpic++ proj07.cpp -o proj07 -lm
+for p in 1 2 4 6 8
+do
+    mpiexec -mca btl self,tcp -np $p ./proj07
+    echo " "
+done
+rm ./proj07
