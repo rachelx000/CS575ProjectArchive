c262c2bd4c07b2e6fa296a5c9642b042014af77d
Rachel Xing <xingru@rabbit.engr.oregonstate.edu>
2025-05-25 22:03:16 -0700

    Update Project6.bash

--- Project6/Project6.bash
+++ Project6/Project6.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-for s in 4096 16384 65536 262144 1048576 4194304
+for s in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304
 do
-        for b in 8 32 64 128 256 512
+        for b in 8 16 32 64 128 256
         do
                 g++ -DDATASIZE=$s -DLOCALSIZE=$b -o proj06 proj06.cpp /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1  -lm -fopenmp
                 ./proj06
